2468828d3ee735f2727efd9a20b78465fe7db823
Dusk Cantuba <duskdenilson.cantuba.cics@ust.edu.ph>
2023-03-11 15:55:41 +0800

    commit (2)

--- P-FA1_Cantuba DD_2ITF.php
+++ P-FA1_Cantuba DD_2ITF.php
@@ -1,7 +1,45 @@
 <?php
 
 class nameOfClass{
-    private $
-    private $
-    private $
+    private $student
+
+    public function setStudent($student){
+        $this->student = $student;
+    }
+
+    public function getStudent{
+        return $this->student;
+    }
+
+    
+    private $section
+
+    public function setSection($section){
+        $this->section = $section;
+    }
+
+    public function getSection{
+        return $this->section;
+    }
+
+
+    private $teacher
+
+    public function setTeacher($teacher){
+        $this->teacher = $teacher;
+    }
+
+    public function getTeacher{
+        return $this->teacher;
+    }
+
+
+
+    public function displayInfo($student, $section, $teacher){
+        echo "Student: " . $this->student . "<br><br>";
+        echo "Section: " . $this->section . "<br><br>";
+        echo "Teacher: " . $this->teacher . "<br><br>";
+    }
 }
+
+?>
